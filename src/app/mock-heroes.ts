7860145427d9e0d13334149ded6a048c02dccbf1
Jose Sánchez Mateo <Sanchez069@gmail.com>
2022-01-19 13:55:35 +0100

    3. Create a feature component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mortadelo' },
+  { id: 2, name: 'Filemon' },
+  { id: 3, name: 'Narco' },
+  { id: 4, name: 'Spiderman' },
+  { id: 5, name: 'Batman' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Venom' },
+  { id: 8, name: 'La Masa' },
+  { id: 9, name: 'Capitán Ámerica' },
+  { id: 10, name: 'Tornado' }
+];
